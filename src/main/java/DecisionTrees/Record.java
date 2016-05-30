4d8d7867426192041bcea6783d41c9c8f4f67e53
Sydnej <sydnej.w@gmail.com>
2016-05-30 10:30:20 +0200

    Drzewo decyzyjne #2

---
+++ src/main/java/DecisionTrees/Record.java
@@ -0,0 +1,15 @@
+package DecisionTrees;
+
+import java.util.*;
+
+public class Record {
+	private ArrayList<DiscreteAttribute> attributes;
+
+	public ArrayList<DiscreteAttribute> getAttributes() {
+		return attributes;
+	}
+	
+	public void setAttributes(ArrayList<DiscreteAttribute> attributes) {
+		this.attributes = attributes;
+	}
+}
